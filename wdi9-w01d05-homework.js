b47be6a60a29c1f31bf6ccfe023b5353d134987a
Daniel Becker <danielbecker@Daniels-MacBook-Pro.local>
2018-09-14 15:39:53 -0600

    wdi9-w01d05-homework.js

---
+++ wdi9-w01d05-homework.js
@@ -0,0 +1,39 @@
+// section 1
+
+// 1. You assign value to a variable by defining it.
+// 2. You change the value of a variable by redefining it.
+// 3. 
+// 4. declaring a variable = naming it, defining a variable = giving the variable a value,
+// assigning a variable = putting that variable in a function.
+// 5. Pseudocoding is when you write our plans for your coding by using plain English words.
+// 6. 75% - 90% thinking about the problem, 10% - 25% coding
+
+// strings
+
+let firstVariable = "hello world";
+firstVariable = 123;
+
+let secondVariable = "hello world";
+secondVariable = "some string";
+
+let yourName = "Daniel Becker";
+console.log(`Hello my name is ${yourName}`);
+
+// Booleans
+
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = 'Kevin';
+
+console.log(a < b);
+console.log(c > d);
+console.log('Name' === 'Name');
+console.log(true !== false);
+console.log(false === false === false === false === false === false === true);
+console.log(false === false)
+console.log(e === 'Kevin');
+console.log(a < b < c);
+console.log(a * a === d);
+console.log(48 !== '48');
